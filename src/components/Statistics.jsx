7352b8cb7d6ab904206774e56d4155df3cdf5ad1
HannaNikola <h.nikolaienko32@gmail.com>
2023-10-18 16:47:08 +0200

    update

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,11 +1,11 @@
 
 import styles from "./Statistics.module.css";
 
-export const Statistics = ({stats}) => {
+export const Statistics = ({title, stats}) => {
   return (
     <section className={styles.conteiner}>
       <div className={styles.statistics}>
-        <h2 className={styles.title}>Upload stats</h2>
+        {title && <h2 className={styles.title}>{title}</h2>}
 
       <ul style={{ listStyle: "none" }} className={styles.statList}>
         {stats.map((item) => (
